80383c6a3e88144ba359a2bf51575f9b8d7e7789
jose luis <jose.l.condori@gmail.com>
2023-01-10 17:56:55 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const section = document.querySelector('section')
+const overlay = document.querySelector('.overlay')
+const showBtn = document.querySelector('.show-modal')
+const closeBtn = document.querySelector('.close-btn');
+  
+
+showBtn.addEventListener('click', () => {
+  section.classList.add('active');
+});
+
+closeBtn.addEventListener('click', () => {
+  section.classList.remove('active');
+});
+overlay.addEventListener('click', () => {
+  section.classList.remove('active');
+});
